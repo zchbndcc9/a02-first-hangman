3fabe63b2dd334f6760bb7978bfa412b5e00e100
Zach Banducci <zbducci@gmail.com>
2018-09-14 10:33:23 -0500

    Removed state and moved it to tally.ex

--- hangman/lib/hangman/game/game.ex
+++ hangman/lib/hangman/game/game.ex
@@ -1,19 +1,6 @@
 defmodule Hangman.Game do
 
-  defmodule State do
-    defstruct(
-      state:      :initializing, 
-      turns_left: 7, 
-      used:       [],
-      letters:    []
-    ) 
-  end
-
-  def new_game() do
-    new_word()
-    |> (fn word -> %Hangman.Game.State{letters: word} end).()
-  end
-
+  def new_game(), do: %Hangman.Game.Tally{}
   def new_word() do
     Dictionary.random_word()
     |> String.split("", trim: true)
