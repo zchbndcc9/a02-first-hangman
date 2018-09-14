5e33fb2b3fbbb5e88f328904af94f2cc7de8475e
Zach Banducci <zbducci@gmail.com>
2018-09-13 19:47:12 -0500

    Added new_word() function that splits word and finished new_game()

--- hangman/lib/hangman/game/game.ex
+++ hangman/lib/hangman/game/game.ex
@@ -4,10 +4,19 @@ defmodule Hangman.Game do
     defstruct(
       state:      :initializing, 
       turns_left: 7, 
-      used:       []
+      used:       [],
+      letters:    []
     ) 
   end
 
-  def new_game(), do: %Hangman.Game.State{}
+  def new_game() do
+    new_word()
+    |> (fn word -> %Hangman.Game.State{letters: word} end).()
+  end
+
+  def new_word() do
+    Dictionary.random_word()
+    |> String.split("", trim: true)
+  end
 
 end
