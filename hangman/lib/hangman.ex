53bb51cfa72aa496e1075265883d79d2ed484f5e
Zach Banducci <zbducci@gmail.com>
2018-09-13 19:24:55 -0500

    added delegation for new_game

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,18 +1,5 @@
 defmodule Hangman do
-  @moduledoc """
-  Documentation for Hangman.
-  """
 
-  @doc """
-  Hello world.
+  defdelegate new_game(), to: Hangman.Game
 
-  ## Examples
-
-      iex> Hangman.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
