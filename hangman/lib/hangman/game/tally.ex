9258826f8d67acf0749141238aabd456591ffde6
Zach Banducci <zbducci@gmail.com>
2018-09-30 16:08:42 -0500

    Removed tally.ex in favor of struct in Hangman.Game module

--- hangman/lib/hangman/game/tally.ex
+++
@@ -1,9 +0,0 @@
-defmodule Hangman.Game.Tally do
-  defstruct(
-    game_state:   :initializing,
-    turns_left:   7,
-    letters:      [],           
-    used:         [],
-    last_guess:   '' 
-  )
-end
