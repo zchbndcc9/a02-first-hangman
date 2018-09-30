ea4ac4c611affb2cb13e489bbf5348e06f5ea5cb
Zach Banducci <zbducci@gmail.com>
2018-09-30 16:10:23 -0500

    Added basic fxnality for initializing struct and letters

--- hangman/lib/hangman/game/game.ex
+++ hangman/lib/hangman/game/game.ex
@@ -1,9 +1,28 @@
 defmodule Hangman.Game do
+  defstruct game_state: :initializing,
+            turns_left: 7,
+            letters: [],
+            used: [],
+            word_to_guess: [],
+            last_guess: ''
 
-  def new_game(), do: %Hangman.Game.Tally{}
+  def new_game() do
+    word = new_word()
+    letters = initialize_letters(word)
+
+    %Hangman.Game{
+      letters: letters,
+      word_to_guess: word
+    }
+  end
+
   def new_word() do
     Dictionary.random_word()
     |> String.split("", trim: true)
   end
 
+  def initialize_letters(word) do
+    Enum.to_list(word)
+    |> Enum.map(fn _x -> '_' end)
+  end
 end
